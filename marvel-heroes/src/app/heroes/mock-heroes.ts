bdbc4d65e859c86039eae9d18ed1a099061fd359
anetastankovska <anetastankovskaane@gmail.com>
2022-09-05 15:02:14 +0200

    Completed heroes app

---
+++ marvel-heroes/src/app/heroes/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "../hero";
+
+export const HEROES: Hero[] = 
+[
+    {id: 12, name: "Captain America"},
+    {id: 13, name: "Black Widow"},
+    {id: 14, name: "Iron Man"},
+    {id: 15, name: "Dr. Strange"},
+    {id: 16, name: "Black Panther"},
+    {id: 17, name: "Hulk"},
+    {id: 18, name: "Thor"},
+    {id: 19, name: "Spiderman"},
+    {id: 20, name: "Loki"}
+];
